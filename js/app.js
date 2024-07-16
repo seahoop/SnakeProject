4ff8996c96e5fad2fe8e203e771b9657d370ed29
ericdu6865 <zhengyuandu2005311@gmail.com>
2024-07-16 13:11:25 -0400

    updated everything

--- js/app.js
+++ js/app.js
@@ -1,25 +1,219 @@
-const cvs = document.getElementByld("canvas");
+const cvs = document.getElementById("canvas");
 const ctx = cvs.getContext("2d");
 
 // Grid for the Arena got the cvs.height/scale from google
-const sclae = 20;
+const scale = 20;
 const rows = cvs.height/scale;
 const columns = cvs.width/scale;
 
 //Game win and lose notices
-let gameProgress;
-const startMenu = document.getElementByld("start-menu")
-const gameOverMenu = document.getElementByld("game-over")
+let gameState;
+const startMenu = document.getElementById("start-menu");
+const gameOverMenu = document.getElementById("game-over");
 
 
+//variable declaration
+let snake;
+let fruit; 
 
 
-//DESIGN of the SNAKE copied from chat gpt and modified 
-this.draw=function(){
-    for(let i = 4; i<this.HTMLDetailsElement.length; i++){
-        ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);
+//Game progress gameover menu
+function setState(state){
+    gameState = state;
+    if(state === "gameover"){
+        gameOverMenu.style.visibility = "visible";
+    } else if (state === "play"){
+        gameOverMenu.style.visibility = "hidden"
+    }   if (state === "start"){
+        startMenu.style.visibility = "visible"
+    }   else if (state !== "start"){
+        startMenu.style.visibility = "hidden"
     }
-// Line below creates square copied from chat gpt and modified 
-    ctx.fillRect(this.x, this.y, scale, scale); 
-    ctx.fillStyle="#5300E2";
+    }
+
+
+
+
+
+
+//Restart funciton
+function restart(){
+    snake.x = 240;
+    snake.y = 220;
+    snake.totalFruit = 0;
+    snake.tail =[];
+    gameOverMenu.style.visibility = "hidden";
+    setState("play")
+    startTune.play();
 }
+function Snake(){
+
+    this.x = 0;
+    this.y = 0;
+    this.xSpeed = scale*1;
+    this.ySpeed = 0;
+    this.totalFruit = 0;
+    this.tail =[];
+
+    //design of snake 
+    this.draw = function() {
+        for (let i = 0; i < this.tail.length; i++) {
+            ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);
+        };
+
+        ctx.fillRect(this.x, this.y, scale, scale);
+        ctx.fillStyle = "#5300E2";
+    };
+
+ //snake position
+ this.update = function(){
+    for(let i=0; i<this.tail.length-1; i++){
+        this.tail[i]=this.tail[i+1];
+    }
+    this.tail[this.totalFruit-1]={x:this.x, y:this.y};
+
+    //border boundaries funciton 
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+
+    if(this.x>cvs.width){
+        setState("gameover")
+    }
+    if(this.x<0){
+        setState("gameover")
+    }
+    if(this.y>cvs.height){
+        setState("gameover")
+    }
+    if(this.y<0){
+        setState("gameover")
+    }
+
+
+
+    }
+    //Key direction programming 
+    this.changeDirection = function(direction){
+        switch(direction){
+            case "Up":
+                if(!this.ySpeed>0){
+                    this.xSpeed = 0;
+                    this.ySpeed = -scale*1;
+                }
+                break;
+                case "Down":
+                    if(!this.ySpeed>0){
+                        this.xSpeed = 0;
+                        this.ySpeed = scale*1;
+                    }
+                    break;
+                case "Left":
+                    if(!this.xSpeed>0){
+                        this.xSpeed = -scale*1;
+                        this.ySpeed = 0;
+                    }
+                    break;
+                    case "Right":
+                        if(!this.xSpeed>0){
+                            this.xSpeed = scale *1;
+                            this.ySpeed = 0;
+
+                        }
+                        break;
+        }
+    };
+    // SNAKE EATING THE FRUIT 
+    this.eat = function(fruit) {
+        if (this.x === fruit.x && this.y === fruit.y) {
+        this.totalFruit++;
+            return true;
+        }
+        return false;
+    };
+    // IF SNAKE COLLIDES WITH ITSELF
+    this.checkForCollision = function() {
+        for (var i=0; i < this.tail.length; i++) {
+            if(this.x === this.tail[i].x && this.y === this.tail[i].y) {
+                this.totalFruit = 0;
+                this.tail = [];
+                setState("gameover"); 
+            }
+        }
+    };
+
+}
+// ~~~~~~~FRUIT~~~~~~~~~
+function Fruit() {
+    this.x;
+    this.y;
+
+    this.randomLocation = function() {
+        this.x = (Math.floor(Math.random() * rows +1) - 1) *scale;
+        this.y = (Math.floor(Math.random() * rows +1) - 1) *scale;
+    };
+
+this.draw = function() {
+    ctx.fillRect(this.x, this.y, scale, scale); 
+    ctx.fillStyle = "#00DB41";
+        ctx.strokeStyle = "#000";
+        ctx.strokeRect(this.x, this.y, scale, scale); 
+        ctx.lineWidth = 2;
+    };
+}
+
+
+// Start snake funciton 
+(function initSnake(){
+
+    snake = new Snake();
+    fruit = new Fruit();
+
+    fruit.randomLocation();
+
+    let speed = window.setInterval(() =>{
+        if (gameState === "play"){
+            ctx.clearRect(0, 0, cvs.width, cvs.height);
+            snake.update();
+            fruit.draw();
+            snake.draw();
+        }
+        if(snake.eat(fruit)){
+            fruit.randomLocation();
+            fruitPop.play();
+        }
+        snake.checkForCollision();
+        document.querySelector("#score").innerText =snake.totalFruit;
+    }, 180);
+    //snake is created moved and cleard every 180 
+    }());
+
+     //Control
+     document.body.onkeyup = function(e){
+        if(e.keyCode == 32){
+            restart();
+        }
+    }
+
+    window.addEventListener("keydown", ((e) =>{
+        const direction = e.key.replace("Arrow", "");
+        snake.changeDirection(direction);
+    }));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
